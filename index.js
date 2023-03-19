fbefa2a9d528fa73c0880d0c745dce41eafbba4c
haruymth <toruha0113@gmail.com>
2023-03-19 17:16:03 +0900

    second commit

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const fetch = require("@replit/node-fetch");
 require("dotenv").config();
 console.log(process.env.key);
-class User{
-    constructor(username,password){
-        console.log(username,password)
-    }
-}
-new User("haru-ymth","aasdf");
 (async()=>{
     const users = await (await fetch("https://thbot2.haru-ymth.repl.co/"+process.env.key)).json();
-    
+    for(let i=0;i<users.length;i++){
+        (async()=>{
+            
+            console.log(users[i])
+            let login = await fetch("https://scratch.mit.edu/accounts/login/",{
+                    headers: {
+                        "x-csrftoken": "a",
+                        "x-requested-with": "XMLHttpRequest",
+                        "Cookie": "scratchcsrftoken=a;scratchlanguage=en;",
+                        "referer": "https://scratch.mit.edu",
+                        "content-type": "application/json"
+                    },
+                    method: "POST",
+                    body:JSON.stringify({
+                        username:users[i].username,
+                        password:users[i].password,
+                        useMessages:true
+                    })
+            });
+            //console.log(login);
+            const sid = login.headers.get("set-cookie");
+            console.log(sid);
+        })();
+    }
 })()
